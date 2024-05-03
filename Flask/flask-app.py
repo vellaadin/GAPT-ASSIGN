edd27a4c3a4919edcad794263029a5b4fa76d22e
JeremyFarrugia <116822093+JeremyFarrugia@users.noreply.github.com>
2024-05-03 13:25:47 +0200

    Started merging work

--- Flask/flask-app.py
+++ Flask/flask-app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_from_directory, request, abort
+from flask import Flask, render_template, send_from_directory, request, abort, jsonify
+import os, json
 
 app = Flask(__name__)
 
@@ -9,6 +10,8 @@ app.template_folder = 'templates' # Set the template folder to the 'templates' f
 URL = "https://teachablemachine.withgoogle.com/models/l4Oo33mJS/"; # URL of the Teachable Machine model
 
 AUTHENTICATION_TOKEN = 'a123' # Authentication token for the user
+
+#-----------------------------------------------------Helper Functions-----------------------------------------------------#
 
 # Authentication function - return True if the user is authorised to access the file, False otherwise
 def authenticate(token):
@@ -24,6 +27,8 @@ def authenticate_and_send_file(token, file_path):
         return send_from_directory('static', file_path)
     else:
         abort(403)
+
+#-----------------------------------------------------Routes-----------------------------------------------------#
 
 # Index page
 @app.route('/')
@@ -58,6 +63,33 @@ def get_url():
         return URL
     else:
         abort(403)
+        
+# Define a route to serve emotion JSON files
+@app.route('/<emotion>')
+def get_emotion_json(emotion):
+    emotion = emotion.lower()
+    
+    # Check if the emotion is valid
+    valid_emotions = ['sad', 'angry', 'disgust', 'fear', 'happy', 'neutral', 'surprise']
+    if emotion not in valid_emotions:
+        return 'Invalid emotion', 400  # Return a 400 Bad Request status for invalid emotions
+
+    # Construct the path to the JSON file
+    json_file_path = os.path.join('static', 'Playlists', f'{emotion}.json')
+    # Append file path to the script directory
+    json_file_path = os.path.join(os.path.dirname(__file__), json_file_path)
+
+    # Check if the JSON file exists
+    if not os.path.exists(json_file_path):
+        print(json_file_path)
+        return 'Emotion data not found', 404  # Return a 404 Not Found status if the file does not exist
+
+    # Return the JSON data
+    with open(json_file_path) as f:
+        data = json.load(f)
+    return jsonify(data)
+
+#-----------------------------------------------------Error Handling-----------------------------------------------------#
 
 # Page to display when content is not found
 @app.errorhandler(404)
@@ -68,6 +100,8 @@ def not_found_error(error):
 @app.errorhandler(403)
 def forbidden_error(error):
     return render_template('forbidden_access.html', error=error), 403
+
+#-----------------------------------------------------Main Function-----------------------------------------------------#
 
 if __name__ == '__main__':
     app.run(debug=True)
